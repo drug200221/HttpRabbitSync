a71b8d82600832d25594607df34ddcc03875cd69
Георгий <drug200221@gmail.com>
2024-11-15 19:05:43 +0300

    исправил получение из .env

--- CheckerHttpEvents/GlobalVariable.cs
+++ CheckerHttpEvents/GlobalVariable.cs
@@ -1,4 +1,3 @@
-using DotNetEnv;
 using RabbitMQ.Client;
 
 namespace CheckerHttpEvents;
@@ -6,12 +5,12 @@ namespace CheckerHttpEvents;
 public static class GlobalVariable
 {
     public static readonly string FilePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "app.config.json");
-    
+
     public static readonly ConnectionFactory ConnectionFactory = new ()
     {
-        HostName = Env.GetString("RABBITMQ_HOSTNAME"),
-        Port = (Env.GetInt("RABBITMQ_PORT")),
-        UserName = Env.GetString("RABBITMQ_DEFAULT_USER"),
-        Password = Env.GetString("RABBITMQ_DEFAULT_PASS")
+        HostName = Environment.GetEnvironmentVariable("RABBITMQ_HOSTNAME"),
+        Port = int.Parse(Environment.GetEnvironmentVariable("RABBITMQ_PORT")),
+        UserName = Environment.GetEnvironmentVariable("RABBITMQ_DEFAULT_USER"),
+        Password = Environment.GetEnvironmentVariable("RABBITMQ_DEFAULT_PASS"),
     };
 }
